4efe2b51c407812d0d3c30f454295b51631cdf34
1502Pam <143039675+1502Pam@users.noreply.github.com>
2023-11-23 20:48:47 -0500

    Update Código_sensorFSR_sensorEMG_Grupo12.ino

--- Software/Código_sensorFSR_sensorEMG_Grupo12.ino
+++ Software/Código_sensorFSR_sensorEMG_Grupo12.ino
@@ -1,19 +1,22 @@
 #define EMG_SENSOR_PIN A4       // Sensor EMG conectado al pin analógico A4
 #define AIR_PUMP_PIN 5          // Bomba conectada al pin digital 5
-#define VALVE1_PIN 2            // Válvula para bomba conectado al pin digital 2
-#define VALVE2_PIN 3            // Válvula para expulsión conectado al pin digital 3
-#define motorPin 6              // Pin del motor digital 6
-#define sensorpin A3            // Pin del sensor FSR analógico 3
+#define VALVE1_PIN 2            // Válvula para microválvulas de aire conectada a bomba
+#define VALVE2_PIN 7            // Válvula para expulsión de aire 
+#define motorPin 6              // Pin del motor 
+#define sensorpin A3            // Pin del sensor FSR
+#define alpha 0.03              // Factor de suavizado
 
-// Variables Sensor EMG
+//Variables del sensor EMG
 int sensor_EMG;
-int contador;                   // Diferenciar entre flexión/extensión
+int contador=1;                 // Diferenciar entre flexión/extensión
 int val_filtrado = 0;
 int sen_analog = 0;
-#define alpha 0.03;             // Factor de suavizado
-const int x = 20;               // Constante entera
+const int x = 24;               // Constante entera
+int periodo= 3000;
+int periodo2= 3000;
+unsigned long tiempoAhora=0;
 
-// Variables Sensor FSR
+// Variables del sensor FSR
 int sensorFSR;
 int tmap;
 int i;
@@ -23,7 +26,9 @@ int promFSR;
 void setup() {
   pinMode(EMG_SENSOR_PIN, INPUT);
   pinMode(AIR_PUMP_PIN, OUTPUT);
+  // Válvula 1
   pinMode(VALVE1_PIN, OUTPUT);
+  // Válvula 2
   pinMode(VALVE2_PIN, OUTPUT);
   pinMode(motorPin, OUTPUT);
   pinMode(sensorpin, INPUT);
@@ -31,8 +36,7 @@ void setup() {
 }
 
 void loop() {
-    
-    contador = 1; 		// Siempre valor inicial = 1
+    //Contador =1;                 // Siempre valor inicial =1
 
     // Sensor FSR
     for (i = 1; i <= 10; i++) {
@@ -42,43 +46,51 @@ void loop() {
     }
     promFSR = auxSuma / 10;
     auxSuma = 0;
-    Serial.print("Sensor FSR: ");
-    Serial.println(sensorFSR);
-    tmap = map(promFSR, 0, 1000, 10, 255);
-    analogWrite(motorPin, tmap);
+  	//Serial.print("Sensor FSR: ");
+    //Serial.println(sensorFSR);
+    // tmap = map(promFSR, 0, 1000, 10, 255);
+    //analogWrite(motorPin, tmap);
   
-  // Filtro EMA (Promedio móvil exponencial) para señal EMG
+    // FILTRO EMA (Promedio móvil exponencial) para señal EMG
     sensor_EMG = analogRead(EMG_SENSOR_PIN);
-    val_filtrado = (alpha * sensor_EMG)+((1-alpha)*val_filtrado); 	//valFiltrado: de valor anterior
-    //Imprimir valor del Sensor EMG
-    Serial.print("Sensor EMG: ");
+    val_filtrado = (alpha * sensor_EMG)+((1-alpha)*val_filtrado); //valFiltrado de valor anterior
+    //Imprimir sensor EMG
+  	//Serial.print("Sensor EMG: ");
     Serial.print(sensor_EMG);
     Serial.print(" , ");
     Serial.println(val_filtrado);
+    //Serial.print(" , ");
+    //Serial.println(contador);
 
-  // Control de la bomba basado en el sensor EMG
-    if (val_filtrado >x && contador=1) {
-    //Se realiza agarre 
+    // Control de la bomba basado en el sensor EMG
+    if (val_filtrado > x && contador == 1) {
+    //se realiza agarre 
       digitalWrite(AIR_PUMP_PIN, HIGH);
-      digitalWrite(VALVE1_PIN, HIGH);				// Válvula abierta
-      digitalWrite(VALVE2_PIN, LOW);				// Válvula cerrada
-      delay(500);
-      digitalWrite(VALVE1_PIN, LOW); 				// Mantener agarre
-      digitalWrite(AIR_PUMP_PIN, LOW); 
-
-      Serial.print("**Cierra Mano**");
-      Serial.print("Contador: ");
-      Serial.print(contador);
-      contador = 2;
+      digitalWrite(VALVE1_PIN, HIGH);                        // Válvula abierta
+      digitalWrite(VALVE2_PIN, LOW);                         // Válvula cerrada
+      if(millis()> tiempoAhora + periodo){                   // Temporizador
+        tiempoAhora=millis();
+        digitalWrite(VALVE1_PIN, LOW);                       // Mantener agarre
+        digitalWrite(AIR_PUMP_PIN, LOW);
+        contador =2;
+      }
+      Serial.println("**Cierra Mano**");
+      //Serial.print("Contador: ");
+      //Serial.println(contador);
+      
     } 
-    else if (val_filtrado >x && contador=2){
+    else if (val_filtrado > x && contador == 2){
       //Realizar extensión del pulgar
-      digitalWrite(VALVE2_PIN, HIGH);				// Válvula abierta
+      digitalWrite(VALVE2_PIN, HIGH);//abierta
       //valvula de la bomba cerrada
       digitalWrite(VALVE1_PIN, LOW);
-      Serial.print("**Abre Mano**");
-      Serial.print("Contador: ");
-      Serial.print(contador);
+      Serial.println("**Abre Mano**");
+      //Serial.print("Contador: ");
+      //Serial.println(contador);
+      if(millis()> tiempoAhora + periodo2){ //temporizador
+         tiempoAhora=millis();
+         contador =1;
+      }
     }
-  delay(500);  
+  delay(10);  
 }
